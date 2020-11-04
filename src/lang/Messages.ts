145b7f2bddc774f38100374b73e4a7e3508c389c
Jérôme Beau <javarome@gmail.com>
2020-11-05 00:49:12 +0100

    Use @rr0/common and @rr0/lang packages

--- src/lang/Messages.ts
+++ src/lang/Messages.ts
@@ -2,10 +2,10 @@ import {WithTimeMessages} from "../time/TimeMessages";
 import {WithEventMessages} from "../time/EventMessages";
 import {WithPlaceMessages} from "../place/PlaceMessages";
 import {WithOrgMessages} from "../org/OrgMessages";
-import {DictionaryMessages} from "./Translator";
 import {WithPeopleMessages} from "../people/PeopleMessages";
+import {KeyValue} from "@rr0/common";
 
 
 export interface Messages extends WithPeopleMessages, WithTimeMessages, WithEventMessages, WithPlaceMessages, WithOrgMessages {
-  dict: DictionaryMessages
+  dict: KeyValue
 }
